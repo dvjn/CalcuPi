f5bce424c9eef8ab678e66cb5e628584384ea731
Divy Jain <dkj@somaiya.edu>
2021-03-16 23:29:06 +0530

    🚨 apply clippy suggestions

--- src/canvas.rs
+++ src/canvas.rs
@@ -8,12 +8,12 @@ fn get_current_ctx_2d(canvas: &ElRef<HtmlCanvasElement>) -> CanvasRenderingConte
 }
 
 pub fn draw_point(canvas: &ElRef<HtmlCanvasElement>, x: f64, y: f64, in_circle: bool) {
-    let ctx = get_current_ctx_2d(&canvas);
+    let ctx = get_current_ctx_2d(canvas);
 
     ctx.begin_path();
     ctx.arc(
-        x * CANVAS_SIZE + CANVAS_MIDDLE,
-        y * CANVAS_SIZE + CANVAS_MIDDLE,
+        x.mul_add(CANVAS_SIZE, CANVAS_MIDDLE),
+        y.mul_add(CANVAS_SIZE, CANVAS_MIDDLE),
         CANVAS_SIZE / 250.0,
         0.0,
         std::f64::consts::PI * 2.0,
@@ -28,14 +28,14 @@ pub fn draw_point(canvas: &ElRef<HtmlCanvasElement>, x: f64, y: f64, in_circle: 
 }
 
 pub fn fade(canvas: &ElRef<HtmlCanvasElement>) {
-    let ctx = get_current_ctx_2d(&canvas);
+    let ctx = get_current_ctx_2d(canvas);
 
     ctx.set_fill_style(&JsValue::from_str(FADE_COLOR));
     ctx.fill_rect(0.0, 0.0, CANVAS_SIZE, CANVAS_SIZE);
 }
 
 pub fn clear(canvas: &ElRef<HtmlCanvasElement>) {
-    let ctx = get_current_ctx_2d(&canvas);
+    let ctx = get_current_ctx_2d(canvas);
 
     ctx.clear_rect(0.0, 0.0, CANVAS_SIZE, CANVAS_SIZE);
 }
--- src/helpers.rs
+++ src/helpers.rs
@@ -7,5 +7,5 @@ pub fn calculate_pi(total_points: usize, points_in_circle: usize) -> Option<f64>
 }
 
 pub fn is_in_circle(x: f64, y: f64) -> bool {
-    x * x + y * y <= 0.25
+    x.mul_add(x, y * y) <= 0.25
 }
