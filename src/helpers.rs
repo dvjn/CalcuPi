4a4fd242698339b39d72db212b5be71957a6f183
Divy Jain <dkj@somaiya.edu>
2021-03-23 02:03:47 +0530

    🚑 fix pi calculation

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,5 +1,5 @@
 pub fn calculate_pi(total_points: usize, points_in_circle: usize) -> Option<f64> {
-    (total_points == 0).then(|| (points_in_circle as f64) / (total_points as f64) * 4.)
+    (total_points != 0).then(|| (points_in_circle as f64) / (total_points as f64) * 4.)
 }
 
 pub fn is_in_circle(x: f64, y: f64) -> bool {
